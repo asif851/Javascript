68d3a6762c0f3ee562319212fa7030a12b5c14d3
asif851 <asifmredha21@gmail.com>
2023-04-09 14:11:56 +0600

    strung-length added

---
+++ string-length.js
@@ -0,0 +1,10 @@
+var firstName="asif";
+var lengthOfString=firstName.length;
+
+var firstString=firstName[0];
+var lastString=firstName[firstName.length-1];
+
+
+console.log(lengthOfString)
+console.log(firstString)
+console.log(lastString)
