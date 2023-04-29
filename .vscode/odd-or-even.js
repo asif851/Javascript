5059537c3adb491bb2a43acf29e1cdac4de7de41
asif851 <asifmredha21@gmail.com>
2023-04-30 01:59:57 +0600

    added odd=or-even

---
+++ .vscode/odd-or-even.js
@@ -0,0 +1,13 @@
+// program to find out the number is odd or even
+
+var num=12;
+
+//var num = prompt("Enter a number: ");
+
+if(num%2 == 0){
+    result="num is even"
+}
+else{
+    result="num is odd"
+}
+console.log(result)
